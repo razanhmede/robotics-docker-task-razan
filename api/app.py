25ba5bcfce2230e6fee4a45ce602c0dd6788aefc
RazanHmede <razan.hmede@lau.edu>
2024-07-10 14:20:03 +0300

    Updates to docker compose file

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from minio.error import S3Error
 import os
 import io
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 minio_client = Minio(
     "minio:9000",
@@ -15,32 +15,33 @@ minio_client = Minio(
 
 bucket_name = "mybucket"
 
+
 @app.route('/store', methods=['POST'])
 def store():
-    data = request.json()
+    data = request.json
     
     try:
-        object_name = data.get("name")
-        content = data.get("content")
-        
-        # Check if the content is a path or a text
-        if os.path.isfile(content):
-            # If content is a file path
-            with open(content, 'rb') as f:
-                minio_client.put_object(bucket_name, object_name, f, os.stat(content).st_size)
-        else:
-            # If content is text data
-            content_bytes = content.encode('utf-8')
-            content_stream = io.BytesIO(content_bytes)
-            minio_client.put_object(bucket_name, object_name, content_stream, len(content_bytes))
-        
-        return jsonify({"message": "Data stored successfully"}), 200
+    	object_name = data.get("name")
+    	content = data.get("content")
+    	
+    	# Check if the content is a path or a text
+    	if (os.path.isfile(content)):
+    		print("Entered in image file")
+    		minio_client.fput_object(bucket_name, object_name, content)
+    		return jsonify({"message": "Data stored successfully"}), 200
+    		
+    	# Transform the content text to
+    	else:
+    		content_bytes = content.encode('utf-8')
+    		content_stream = io.BytesIO(content_bytes)
+    		length = len(content)
+    		minio_client.put_object(bucket_name, object_name, content_stream, len(content))
+    		return jsonify({"message": "Data stored successfully"}), 200
     except S3Error as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     if not minio_client.bucket_exists(bucket_name):
         minio_client.make_bucket(bucket_name)
     app.run(host='0.0.0.0', port=5000)
-
 
